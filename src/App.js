4fa3f180d00f7eb1f1c78e899c95bd4627161f98
Lymmichael <129142623+Lymmichael@users.noreply.github.com>
2024-01-06 04:57:01 +0800

    update icon caption and update app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import ResumePage from "./page/ResumePage";
 import HomePage from "./page/HomePage";
 import ProjectPage from "./page/ProjectPage";
 import AboutMe from "./page/AboutMe";
 import ContactPage from "./page/ContactPage";
 function App() {
-  // const router = createHashRouter([
-  //   {
-  //     path: "/michaellaw_web",
-  //     element: <HomePage />,
-  //     children: [
-  //       {
-  //         path: "/michaellaw_web/project",
-  //         element: <ProjectPage />,
-  //       },
-  //       {
-  //         path: "/michaellaw_web/resume",
-  //         element: <ResumePage />,
-  //       },
-  //       {
-  //         path: "/michaellaw_web/AboutMe",
-  //         element: <AboutMe />,
-  //       },
-  //       {
-  //         path: "/michaellaw_web/Contact",
-  //         element: <ContactPage />,
-  //       },
-  //     ],
-  //   },
-  // ]);
+  
   return (
     <Router>
       <Routes>
@@ -40,14 +17,6 @@ function App() {
         <Route path="/michaellaw_web/Contact" element={<ContactPage />}/>
       </Routes>
     </Router>
-
-    // <div>
-    //   <>
-    //     ReactDOM.createRoot(document.getElementById("root")).render(
-    //       <RouterProvider router={router} />
-    //     );
-    //   </>
-    // </div>
   );
 }
 
