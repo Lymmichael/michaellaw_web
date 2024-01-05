b4b5f99f29e862abc5801834419f46e089bb809b
Lymmichael <129142623+Lymmichael@users.noreply.github.com>
2024-01-06 03:45:52 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ResumePage from "./page/ResumePage";
 import HomePage from "./page/HomePage";
 import ProjectPage from "./page/ProjectPage";
@@ -33,12 +33,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/michaellaw_web" element={<HomePage />} />
-        <Route path="/michaellaw_web/resume" element={<ResumePage />} />
-        <Route path="/michaellaw_web/project" element={<ProjectPage />} />
+        <Route path="/michaellaw_web/" element={<HomePage />} />
+        <Route path="/michaellaw_web/Resume" element={<ResumePage />} />
+        <Route path="/michaellaw_web/Project" element={<ProjectPage />} />
         <Route path="/michaellaw_web/AboutMe" element={<AboutMe />} />
         <Route path="/michaellaw_web/Contact" element={<ContactPage />}/>
-        
       </Routes>
     </Router>
 
